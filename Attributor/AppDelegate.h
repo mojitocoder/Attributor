31273c7c2f545947752942f7dc47cd3dab4c5071
Quynh Nguyen <qn@rosslynanalytics.com>
2014-09-25 08:19:36 +0100

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Quynh on 25/09/2014.
+//  Copyright (c) 2014 Quynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Quynh on 25/09/2014.
+//  Copyright (c) 2014 Quynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
